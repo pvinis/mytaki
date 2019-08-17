ed60f3661a9b29473bee01c772d31af01b0db9ee
Pavlos Vinieratos <pvinis@gmail.com>
2019-08-18 01:42:32 +0300

    batman

---
+++ index.ts
@@ -0,0 +1,50 @@
+import * as R from 'ramda'
+
+
+const Player = (minionA, minionB, minionC) => {
+
+    return {
+        m: { a: minionA, b: minionB, c: minionC, EMPTY: 0 }
+    }
+}
+
+
+const bottomPlayer = Player(1, 2, 3)
+
+const {m } = bottomPlayer
+
+const Grid = (config, initialGrid) => {
+    let grid = initialGrid
+
+
+    return {
+        print: () => {
+            return R.join('\n', grid)
+
+        },
+        fix: () => {
+            grid = [
+                [m.a,     m.b,     m.a    , m.EMPTY],
+                [m.c,     m.c,     m.EMPTY, m.EMPTY],
+                [m.EMPTY, m.EMPTY, m.EMPTY, m.EMPTY],
+            ]
+        }
+    }
+}
+
+
+const bottom = Grid({rows: 3, columns: 4}, [
+    [m.a,     m.b,     m.EMPTY, m.EMPTY],
+    [m.c,     m.c,     m.a    , m.EMPTY],
+    [m.EMPTY, m.EMPTY, m.EMPTY, m.EMPTY],
+])
+
+
+console.log(bottom.print())
+
+bottom.fix()
+
+console.log(bottom.print())
+
+
+
